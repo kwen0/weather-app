879b0fac7ff631d38be07c0359475f47e9737773
Karen <karen.wen3@gmail.com>
2021-12-24 13:29:48 -0500

    completed search, fetch data, and render data functions

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-const location = document.querySelector('#location')
-const cityname = document.querySelector('#cityname')
-const currenttemp = document.querySelector('#currenttemp')
+const searchForm = document.querySelector('#searchform')
+const city = document.querySelector('#city')
+const currentCity = document.querySelector('#currentcity')
+const currentTemp = document.querySelector('#currenttemp')
 const icon = document.querySelector('#icon')
 const condition = document.querySelector('#condition')
 const high = document.querySelector('#high')
 const low = document.querySelector('#low')
+const fahrenheitBtn = document.querySelector('#F')
+const celsiusBtn = document.querySelector('#C')
 
 
-async function getData(location, unit) {
-    try {
-        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&APPID=10f76607761969e3dd7bf41fb74404f6`)
-        let data = await response.json()
-        console.log(data)
-    } catch //throw error
+function storeData(currentCity, currentTemp, icon, condition, high, low) {
+    return { currentCity, currentTemp, icon, condition, high, low }
 }
 
-getData("brooklyn", "imperial")
+async function getData(place, unit) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${unit}&APPID=10f76607761969e3dd7bf41fb74404f6`)
+    let data = await response.json()
+    return cityData = storeData(data.name, Math.round(data.main.temp), data.weather[0].icon, data.weather[0].description, Math.round(data.main.temp_max), Math.round(data.main.temp_min))
+}
+
+searchForm.addEventListener("submit", async e => {
+    e.preventDefault();
+    await getData(city.value, "imperial")
+    renderData(cityData)
+    city.value = null;
+})
+
+function renderData(info) {
+    currentCity.textContent = info.currentCity
+    currentTemp.textContent = `${info.currentTemp}°`
+    condition.textContent = info.condition
+    high.textContent = `H: ${info.high}°`
+    low.textContent = `L: ${info.low}°`
+}
